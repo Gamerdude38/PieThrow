3abdd926b26576b1ee06d00f9cdcc52f8e12a5f6
Gamerdude38 <43382258+Gamerdude38@users.noreply.github.com>
2019-05-28 00:34:58 -0400

    Restructured Files, Added Toon Movement with Animations
    Cogs also have life meters that reflect their health, but their animations have not been set up yet.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ Handlers, Audio, Collisions, and basic health-bar game logic
 
 from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase
+from direct.actor.Actor import Actor
+from direct.task import Task
 from random_cog import RandomCog
 import sys,os
 
@@ -15,18 +17,214 @@ class PieThrow(ShowBase):
 	def __init__(self):
 		#Initialize the Panda window & disable the default mouse controls
 		ShowBase.__init__(self)
-		#self.disableMouse()
-		self.oobe()
+		self.disableMouse()
+		#self.oobe()
 		
 		#Establish where the current directory of the running file is
 		self.currentDirectory = os.path.abspath(sys.path[0])
 		self.pandaDirectory = Filename.fromOsSpecific(self.currentDirectory).getFullpath()
 		
+		#Define variables, particularly to tell if the model is moving
+		self.isThrowing = False
+		self.isMovingInY = False
+		self.isTurning = False
+		self.movementHeading = ''
+		self.turnHeading = ''
+		self.speed = 0.0
+		self.turnSpeed = 0.0
+		
 		#Load the main terrain to be used
 		self.terrain = self.loader.loadModel(self.pandaDirectory + '/resources/terrain/LawbotPlaza.bam')
 		self.terrain.reparentTo(self.render)
 		
-		self.cog = RandomCog()
+		#Initialize the toon model
+		self.toon = self.initToon()
+		self.toon.setZ(-68)
+		
+		#Reparent the camera
+		self.camera.reparentTo(self.toon)
+		self.camera.setPos(self.toon, 0, -20, 5)
+		
+		#Initialize animations
+		self.toon.loop('neutral', 'torso')
+		self.toon.loop('neutral', 'legs')
+		
+		#Define Y (Forwards/backwards) movement
+		self.accept('arrow_up', self.moveInYStart, ['forward'])
+		#self.accept('arrow_up-repeat', self.moveInY, ['forward'])
+		self.accept('arrow_up-up', self.moveInYEnd, ['forward'])
+		self.accept('arrow_down', self.moveInYStart, ['backward'])
+		#self.accept('arrow_down-repeat', self.moveInY, ['backward'])
+		self.accept('arrow_down-up', self.moveInYEnd, ['backward'])
+		
+		#Define turning movement
+		self.accept('arrow_right', self.turnStart, ['right'])
+		#self.accept('arrow_right-repeat', self.turn, ['right'])
+		self.accept('arrow_right-up', self.turnEnd, ['right'])
+		self.accept('arrow_left', self.turnStart, ['left'])
+		#self.accept('arrow_left-repeat', self.turn, ['left'])
+		self.accept('arrow_left-up', self.turnEnd, ['left'])
+		
+		#Pie throwing key, 'control' is the keyName
+		
+		#Tell the taskmanager to keep track of this task
+		self.taskMgr.add(self.updateToon, 'Update Toon')
+		
+	def updateToon(self, task):
+		if self.isMovingInY:
+			self.toon.setY(self.toon, self.speed)
+			
+		if self.isTurning:
+			self.toon.setH(self.toon, self.turnSpeed)	
+		return task.cont
+	
+	def turnStart(self, direction):
+		#If the toon is already turning, ignore the new request
+		if self.isTurning:
+			return
+		
+		#Set the object heading to prevent pressing the right and left keys at the same time
+		self.turnHeading = direction
+		
+		#If the toon is moving in the Y direction...
+		if self.isMovingInY:
+			#And the toon is moving backwards...
+			if self.speed < 0:
+				#Replace the run animation with walk and play it backwards
+				self.toon.setPlayRate(-1, 'walk')
+				
+		#If the toon is not moving
+		else:
+			#Play the forward plain walking animations
+			self.toon.setPlayRate(1, 'walk')
+			self.toon.loop('walk', 'torso')
+			self.toon.loop('walk', 'legs')
+		
+		#Determine which heading the toon will turn
+		if direction == 'right':
+			self.turnSpeed = -0.7
+		elif direction == 'left':
+			self.turnSpeed = 0.7
+		
+		#Tell task manager to start turning
+		self.isTurning = True
+		
+	def turn(self, direction):
+		print()
+		
+	def turnEnd(self, direction):
+		#If the requested direction conflicts with the heading, ignore the request
+		if direction != self.turnHeading:
+			return
+			
+		#If the toon is not moving in the Y direction...
+		if not self.isMovingInY:
+			#Make the animation play neutral before stopping to turn
+			self.toon.loop('neutral', 'torso')
+			self.toon.loop('neutral', 'legs')
+		
+		#Tell task manager to stop turning
+		self.isTurning = False
+		
+	def moveInYStart(self, direction):
+		#If the toon is already moving, ignore the new request
+		if self.isMovingInY:
+			return
+		
+		#Set the object heading to prevent pressing the up and down keys at the same time
+		self.movementHeading = direction
+		
+		#If the up key is pressed...
+		if direction == 'forward':
+			#Set a positive speed and make the toon run
+			self.speed = 0.6
+			self.toon.loop('run', 'torso')
+			self.toon.loop('run', 'legs')
+		
+		#Otherwise...
+		elif direction == 'backward':
+			#Set a negative speed and make the toon walk backwards
+			self.speed = -0.3
+			self.toon.setPlayRate(-1, 'walk')
+			self.toon.loop('walk', 'torso')
+			self.toon.loop('walk', 'legs')
+		
+		#Tell the task manager to start moving in local Y
+		self.isMovingInY = True
+		
+	def moveInY(self, direction):
+		print()
+			
+	def moveInYEnd(self, direction):
+		#If the requested direction conflicts with the heading, ignore the request
+		if direction != self.movementHeading:
+			return
+		
+		#If the toon is not turning...
+		if not self.isTurning:
+			#Set the speed to positive and have the toon idle
+			self.speed = 0.6
+			self.toon.loop('neutral', 'torso')
+			self.toon.loop('neutral', 'legs')
+			
+		#Otherwise...
+		else:
+			#Make the toon walk instead
+			self.toon.setPlayRate(1, 'walk')
+			self.speed = 0.6
+			self.toon.loop('walk', 'torso')
+			self.toon.loop('walk', 'legs')
+		
+		#Tell the task manager to stop moving in local Y
+		self.isMovingInY = False
+		
+	def initToon(self):
+		#Create the toon!
+		self.toon = Actor({'torso': self.pandaDirectory + '/resources/toon/models/tt_a_chr_dgl_shorts_torso_1000.bam',
+							'legs': self.pandaDirectory + '/resources/toon/models/tt_a_chr_dgm_shorts_legs_1000.bam'},
+							{'torso':{
+							'neutral': self.pandaDirectory + '/resources/toon/animations/tt_a_chr_dgl_shorts_torso_neutral.bam',
+							'run': self.pandaDirectory + '/resources/toon/animations/tt_a_chr_dgl_shorts_torso_run.bam',
+							'attack': self.pandaDirectory + '/resources/toon/animations/tt_a_chr_dgl_shorts_torso_pie-throw.bam',
+							'walk': self.pandaDirectory + '/resources/toon/animations/tt_a_chr_dgl_shorts_torso_walk.bam'
+							},
+							'legs':{
+							'neutral': self.pandaDirectory + '/resources/toon/animations/tt_a_chr_dgm_shorts_legs_neutral.bam',
+							'run': self.pandaDirectory + '/resources/toon/animations/tt_a_chr_dgm_shorts_legs_run.bam',
+							'attack': self.pandaDirectory + '/resources/toon/animations/tt_a_chr_dgm_shorts_legs_pie-throw.bam',
+							'walk': self.pandaDirectory + '/resources/toon/animations/tt_a_chr_dgm_shorts_legs_walk.bam'
+							}})
+		self.toon.attach('torso', 'legs', 'joint_hips')
+		self.toon.reparentTo(render)
+		self.toon.find('**/neck').setColor(1, 1, 0)
+		self.toon.find('**/legs').setColor(1, 1, 0)
+		self.toon.find('**/arms').setColor(1, 1, 0)
+		self.toon.find('**/hands').setColor(1, 1, 1)
+		
+		#Set textures and remove unnecessary models
+		self.toon.find('**/sleeves').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/toon/textures/ttr_t_chr_avt_shirtSleeve_cashbotCrusher.jpg'),1)
+		self.toon.find('**/torso-top').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/toon/textures/ttr_t_chr_avt_shirt_cashbotCrusher.jpg'),1)
+		self.toon.find('**/torso-bot').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/toon/textures/ttr_t_chr_avt_shorts_cashbotCrusher.jpg'),1)
+		self.toon.find('**/shoes').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/toon/textures/ttr_t_chr_avt_acc_sho_cashbotCrusher.jpg'),1)
+		self.toon.find('**/feet').removeNode()
+		self.toon.find('**/boots_short').removeNode()
+		self.toon.find('**/boots_long').removeNode()
+		
+		#Create the toon head!
+		self.toonHead = loader.loadModel(self.pandaDirectory + '/resources/toon/models/tt_a_chr_dgm_skirt_head_1000.bam')
+		self.toonHead.reparentTo(self.toon.find('**/def_head'))
+		self.toonHead.find('**/head').setColor(1, 1, 0)
+		self.toonHead.find('**/head-front').setColor(1, 1, 0)
+		
+		#Add a cute hat
+		self.topHat = loader.loadModel(self.pandaDirectory + '/resources/toon/models/tt_m_chr_avt_acc_hat_topHat.bam')
+		self.topHat.reparentTo(self.toonHead.find('**/head'))
+		self.topHat.setZ(0.5)
+		self.topHat.setHpr(180,-45,0)
+		self.topHat.setTexture(loader.loadTexture(self.pandaDirectory + '/resources/toon/textures/tt_t_chr_avt_acc_hat_topHatQuizmaster.jpg'),1)
+		self.topHat.setScale(0.35)
+		
+		return self.toon
 		
 pieThrow = PieThrow()
 pieThrow.run()
--- random_cog.py
+++ random_cog.py
@@ -16,7 +16,8 @@ import random
 class RandomCog():
 	def __init__(self):
 		#Define variables for the cog's health and speed
-		self.health = 5
+		self.maxHealth = 5
+		self.currentHealth = 5
 		self.speed = 1
 		
 		#Establish current file path location
@@ -24,7 +25,7 @@ class RandomCog():
 		self.pandaDirectory = Filename.fromOsSpecific(self.currentDirectory).getFullpath()
 		
 		#Generate a random number between 0 and 31 inclusive to select a cog
-		self.randomNumber = random.randint(0, 32)
+		self.randomNumber = random.randint(0, 31)
 		
 		#Num cogs in suits
 		#A - 14
@@ -34,6 +35,7 @@ class RandomCog():
 		#Define boolean for flunky glasses
 		self.hasGlasses = False
 		
+		#Determine which cog the random number generator picked and set its properties
 		if self.randomNumber <= 13:
 			#Render the suit A type cogs
 			self.cog = Actor(self.pandaDirectory + '/resources/cogs/models/tt_a_ene_cga_zero.bam',{
@@ -47,37 +49,37 @@ class RandomCog():
 			
 			if self.randomNumber == 0:
 				self.head = self.headList.find('**/backstabber')
-				self.cog.find('**/hands').setColor(223,193,231)
+				self.cog.find('**/hands').setColorScale(0.75,0.75,0.95,1)
 				self.cog.setScale(0.7)
 				self.setLawTexture()
 				
 			elif self.randomNumber == 1:
 				self.head = self.headList.find('**/bigcheese')
-				self.cog.find('**/hands').setColor(180,249,164)
+				self.cog.find('**/hands').setColor(0.65,0.95,0.85,1)
 				self.cog.setScale(1)
 				self.setBossTexture()
 				
 			elif self.randomNumber == 2:
 				self.head = self.headList.find('**/bigwig')
-				self.cog.find('**/hands').setColor(223,193,231)
+				self.cog.find('**/hands').setColor(0.75,0.75,0.95,1)
 				self.cog.setScale(1)
 				self.setLawTexture()
 				
 			elif self.randomNumber == 3:
 				self.head = self.headList.find('**/headhunter')
-				self.cog.find('**/hands').setColor(253,172,173)
+				self.cog.find('**/hands').setColor(0.95,0.75,0.75,1)
 				self.cog.setScale(0.85)
 				self.setBossTexture()
 				
 			elif self.randomNumber == 4:
 				self.head = self.headList.find('**/legaleagle')
-				self.cog.find('**/hands').setColor(89,54,123)
+				self.cog.find('**/hands').setColor(0.3,0.3,0.55,1)
 				self.cog.setScale(0.9)
 				self.setLawTexture()
 				
 			elif self.randomNumber == 5:
 				self.head = self.headList.find('**/numbercruncher')
-				self.cog.find('**/hands').setColor(133,244,180)
+				self.cog.find('**/hands').setColor(0.65,0.95,0.85,1)
 				self.cog.setScale(0.8)
 				self.setCashTexture()
 				
@@ -86,19 +88,19 @@ class RandomCog():
 				self.head = self.headList.find('**/numbercruncher')
 				self.head.setTexture(loader.loadTexture(self.pandaDirectory + \
 									'/resources/cogs/textures/name-dropper.jpg'), 1)
-				self.cog.find('**/hands').setColor(248,176,216)
+				self.cog.find('**/hands').setColor(0.95,0.75,0.95,1)
 				self.cog.setScale(0.7)
 				self.setSellTexture()
 				
 			elif self.randomNumber == 7:
 				self.head = self.headList.find('**/pennypincher')
-				self.cog.find('**/hands').setColor(251,140,144)
+				self.cog.find('**/hands').setColor(0.98,0.55,0.56,1)
 				self.cog.setScale(0.6)
 				self.setCashTexture()
 				
 			elif self.randomNumber == 8:
 				self.head = self.headList.find('**/yesman')
-				self.cog.find('**/hands').setColor(253,172,173)
+				self.cog.find('**/hands').setColor(0.95,0.75,0.75,1)
 				self.cog.setScale(0.7)
 				self.setBossTexture()
 				
@@ -107,20 +109,20 @@ class RandomCog():
 				self.head = self.headList.find('**/yesman')
 				self.head.setTexture(loader.loadTexture(self.pandaDirectory + \
 									'/resources/cogs/textures/robber-baron.jpg'), 1)
-				self.cog.find('**/hands').setColor(133,244,180)
+				self.cog.find('**/hands').setColor(0.65,0.95,0.85,1)
 				self.cog.setScale(1)
 				self.setCashTexture()
 				
 			elif self.randomNumber == 10:
 				#mr. hollywood
 				self.head = self.headList.find('**/yesman')
-				self.cog.find('**/hands').setColor(248,176,216)
+				self.cog.find('**/hands').setColor(0.95,0.75,0.95,1)
 				self.cog.setScale(1)
 				self.setSellTexture()
 				
 			elif self.randomNumber == 11:
 				self.head = self.headList.find('**/twoface')
-				self.cog.find('**/hands').setColor(248,176,216)
+				self.cog.find('**/hands').setColor(0.95,0.75,0.95,1)
 				self.cog.setScale(0.8)
 				self.setSellTexture()
 				
@@ -129,7 +131,7 @@ class RandomCog():
 				self.head = self.headList.find('**/twoface')
 				self.head.setTexture(loader.loadTexture(self.pandaDirectory + \
 									'/resources/cogs/textures/mingler.jpg'), 1)
-				self.cog.find('**/hands').setColor(248,176,216)
+				self.cog.find('**/hands').setColor(0.95,0.75,0.95,1)
 				self.cog.setScale(0.85)
 				self.setSellTexture()
 				
@@ -138,7 +140,7 @@ class RandomCog():
 				self.head = self.headList.find('**/twoface')
 				self.head.setTexture(loader.loadTexture(self.pandaDirectory + \
 									'/resources/cogs/textures/double-talker.jpg'), 1)
-				self.cog.find('**/hands').setColor(223,193,231)
+				self.cog.find('**/hands').setColor(0.75,0.75,0.95,1)
 				self.cog.setScale(0.7)
 				self.setLawTexture()
 				
@@ -156,31 +158,32 @@ class RandomCog():
 			
 			if self.randomNumber == 14:
 				self.head = self.headList.find('**/ambulancechaser')
-				self.cog.find('**/hands').setColor(223,193,231)
+				self.cog.find('**/hands').setColor(0.75,0.75,0.95,1)
 				self.cog.setScale(0.8)
 				self.setLawTexture()
 				
 			elif self.randomNumber == 15:
 				self.head = self.headList.find('**/beancounter')
-				self.cog.find('**/hands').setColor(133,244,180)
+				self.cog.find('**/hands').setColor(0.65,0.95,0.85,1)
 				self.cog.setScale(0.7)
 				self.setCashTexture()
 			
 			elif self.randomNumber == 16:
+				#downsizer
 				self.head = self.headList.find('**/beancounter')
-				self.cog.find('**/hands').setColor(253,172,173)
+				self.cog.find('**/hands').setColor(0.95,0.75,0.75,1)
 				self.cog.setScale(0.8)
 				self.setBossTexture()
 				
 			elif self.randomNumber == 17:
 				self.head = self.headList.find('**/loanshark')
-				self.cog.find('**/hands').setColor(133,244,180)
+				self.cog.find('**/hands').setColor(0.65,0.95,0.85,1)
 				self.cog.setScale(1)
 				self.setCashTexture()
 				
 			elif self.randomNumber == 18:
 				self.head = self.headList.find('**/movershaker')
-				self.cog.find('**/hands').setColor(248,176,216)
+				self.cog.find('**/hands').setColor(0.95,0.75,0.95,1)
 				self.cog.setScale(0.8)
 				self.setSellTexture()
 				
@@ -194,13 +197,13 @@ class RandomCog():
 				
 			elif self.randomNumber == 20:
 				self.head = self.headList.find('**/pencilpusher')
-				self.cog.find('**/hands').setColor(253,172,173)
+				self.cog.find('**/hands').setColor(0.95,0.75,0.75,1)
 				self.cog.setScale(0.6)
 				self.setBossTexture()
 				
 			elif self.randomNumber == 21:
 				self.head = self.headList.find('**/telemarketer')
-				self.cog.find('**/hands').setColor(248,176,216)
+				self.cog.find('**/hands').setColor(0.95,0.75,0.95,1)
 				self.cog.setScale(0.6)
 				self.setSellTexture()
 				
@@ -209,7 +212,7 @@ class RandomCog():
 				self.head = self.headList.find('**/telemarketer')
 				self.head.setTexture(loader.loadTexture(self.pandaDirectory + \
 									'/resources/cogs/textures/spin-doctor.jpg'), 1)
-				self.cog.find('**/hands').setColor(133,244,180)
+				self.cog.find('**/hands').setColor(0.65,0.95,0.85,1)
 				self.cog.setScale(0.9)
 				self.setLawTexture()
 			
@@ -227,7 +230,7 @@ class RandomCog():
 			if self.randomNumber == 23:
 				#actually a short change
 				self.head = self.headList.find('**/coldcaller')
-				self.cog.find('**/hands').setColor(133,244,180)
+				self.cog.find('**/hands').setColor(0.65,0.95,0.85,1)
 				self.cog.setScale(0.8)
 				self.setCashTexture()
 				
@@ -235,14 +238,14 @@ class RandomCog():
 				#cold caller needs to be recolored
 				self.head = self.headList.find('**/coldcaller')
 				self.head.setColor(0, 0, 255, 1)
-				self.cog.find('**/hands').setColor(22,123,241)
+				self.cog.find('**/hands').setColor(0.09,0.48,0.95,1)
 				self.cog.setScale(0.8)
 				self.setSellTexture()
 				
 			elif self.randomNumber == 25:
 				self.head = self.headList.find('**/flunky')
-				hasGlasses = True
-				self.cog.find('**/hands').setColor(253,172,173)
+				self.hasGlasses = True
+				self.cog.find('**/hands').setColor(0.95,0.75,0.75,1)
 				self.cog.setScale(0.8)
 				self.setBossTexture()
 				
@@ -251,31 +254,31 @@ class RandomCog():
 				self.head = self.headList.find('**/flunky')
 				self.head.setTexture(loader.loadTexture(self.pandaDirectory + \
 									'/resources/cogs/textures/corporate-raider.jpg'), 1)
-				self.cog.find('**/hands').setColor(253,152,153)
+				self.cog.find('**/hands').setColor(0.98,0.55,0.56,1)
 				self.cog.setScale(1.2)
 				self.setBossTexture()
 				
 			elif self.randomNumber == 27:
 				self.head = self.headList.find('**/gladhander')
-				self.cog.find('**/hands').setColor(248,176,216)
+				self.cog.find('**/hands').setColor(0.95,0.75,0.95,1)
 				self.cog.setScale(1)
 				self.setSellTexture()
 				
 			elif self.randomNumber == 28:
 				self.head = self.headList.find('**/micromanager')
-				self.cog.find('**/hands').setColor(253,172,173)
+				self.cog.find('**/hands').setColor(0.95,0.75,0.75,1)
 				self.cog.setScale(0.5)
 				self.setBossTexture()
 				
 			elif self.randomNumber == 29:
 				self.head = self.headList.find('**/moneybags')
-				self.cog.find('**/hands').setColor(133,244,180)
+				self.cog.find('**/hands').setColor(0.65,0.95,0.85,1)
 				self.cog.setScale(1.1)
 				self.setCashTexture()
 				
 			elif self.randomNumber == 30:
 				self.head = self.headList.find('**/tightwad')
-				self.cog.find('**/hands').setColor(133,244,180)
+				self.cog.find('**/hands').setColor(0.65,0.95,0.85,1)
 				self.cog.setScale(0.9)
 				self.setCashTexture()
 				
@@ -284,7 +287,7 @@ class RandomCog():
 				self.head = self.headList.find('**/tightwad')
 				self.head.setTexture(loader.loadTexture(self.pandaDirectory + \
 									'/resources/cogs/textures/bottom-feeder.jpg'), 1)
-				self.cog.find('**/hands').setColor(223,193,231)
+				self.cog.find('**/hands').setColor(0.75,0.75,0.95,1)
 				self.cog.setScale(0.8)
 				self.setLawTexture()
 		else:
@@ -294,19 +297,35 @@ class RandomCog():
 		self.cog.reparentTo(render)
 		self.head.reparentTo(self.cog.find('**/def_head'))
 		
+		#Attach the glasses, if necessary
 		if self.hasGlasses:
 			self.glasses = self.headList.find('**/glasses')
 			self.glasses.reparentTo(self.head)
 		
-		self.lifeMeter = loader.loadModel(self.pandaDirectory + '/resources/cogs/models/cog_life_meter.bam')
+		'''
+		After doing some research into using the life meter, I found a class from Toontown Empire that
+		was particularly helpful. https://github.com/AnonymousDeveloper13/Toontown-Empire/blob/master/src/toontown/suit/SuitHealthBar.py
+		'''
+		
+		#Set up life meter
+		self.lifeMeter = loader.loadModel(self.pandaDirectory + '/resources/cogs/models/matching_game_gui.bam').find('**/minnieCircle')
 		self.lifeMeter.reparentTo(self.cog.find('**/def_joint_attachMeter'))
-		self.lifeMeter.setHpr(180, 0, 0)
-		self.lifeMeter.setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/cog-life-meter-big.png'))
+		self.lifeMeter.setHpr(180, 0.8, 0)
+		self.lifeMeter.setY(0.02)
+		self.lifeMeter.setScale(3)
+		
+		self.lifeMeterGlow = loader.loadModel(self.pandaDirectory + '/resources/cogs/models/glow.bam')
+		self.lifeMeterGlow.reparentTo(self.lifeMeter)
+		self.lifeMeterGlow.setScale(0.25)
+		self.lifeMeterGlow.setPos(-0.01, 0.01, 0.02)
+		
+		self.updateHealth()
 		
 		self.cog.loop('neutral')
 		
 		
 	def setSellTexture(self):
+		#Set the suit texture to the Sellbot texture
 		self.cog.findAllMatches('**/torso').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/s_blazer.jpg'), 1)
 		
 		self.cog.findAllMatches('**/arms').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/s_sleeve.jpg'), 1)
@@ -314,6 +333,7 @@ class RandomCog():
 		self.cog.findAllMatches('**/legs').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/s_leg.jpg'), 1)
 	
 	def setCashTexture(self):
+		#Set the suit texture to the Cashbot texture
 		self.cog.findAllMatches('**/torso').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/m_blazer.jpg'), 1)
 		
 		self.cog.findAllMatches('**/arms').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/m_sleeve.jpg'), 1)
@@ -321,6 +341,7 @@ class RandomCog():
 		self.cog.findAllMatches('**/legs').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/m_leg.jpg'), 1)
 		
 	def setLawTexture(self):
+		#Set the suit texture to the Lawbot texture
 		self.cog.findAllMatches('**/torso').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/l_blazer.jpg'), 1)
 		
 		self.cog.findAllMatches('**/arms').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/l_sleeve.jpg'), 1)
@@ -328,8 +349,29 @@ class RandomCog():
 		self.cog.findAllMatches('**/legs').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/l_leg.jpg'), 1)
 	
 	def setBossTexture(self):
+		#Set the suit texture to the Bossbot texture
 		self.cog.findAllMatches('**/torso').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/c_blazer.jpg'), 1)
 		
 		self.cog.findAllMatches('**/arms').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/c_sleeve.jpg'), 1)
 		
 		self.cog.findAllMatches('**/legs').setTexture(loader.loadTexture(self.pandaDirectory + '/resources/cogs/textures/c_leg.jpg'), 1)
+		
+	def updateHealth(self):
+		#Update the life meter, depending on the cog's health
+		if self.currentHealth / self.maxHealth >= 0.95:
+			self.lifeMeter.setColor(0, 1, 0)
+		elif self.currentHealth / self.maxHealth >= 0.65:
+			self.lifeMeter.setColor(1, 1, 0)
+		elif self.currentHealth / self.maxHealth >= 0.35:
+			self.lifeMeter.setColor(1, 0.5, 0)
+		elif self.currentHealth / self.maxHealth >= 0.05:
+			self.lifeMeter.setColor(1, 0, 0)
+		elif self.currentHealth / self.maxHealth >= 0:
+			print('Blinking task!')
+		else:
+			self.destruct()	
+		
+	
+	def destruct(self):
+		#Play the destruction animation, then remove the model
+		print('BOOM!')
